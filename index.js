f31f93bde388d02a9b36773bbfd997454677ab36
Erfath <erfathparvez1@gmail.com>
2023-01-26 14:45:38 +0600

    from new pc

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
+        console.log('db connected')
         const itemCollection = client.db('inventory').collection('item');
        
         app.get('/inventory', async (req, res)=> {
